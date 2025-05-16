e6eb96b7d0a6e7c83bb09158b75be0d9fd357ffe
David <david.irusta@itscordoba.edu.ar>
2025-05-16 20:00:16 -0300

    Merge branch 'main' of https://github.com/David-irusta/David-

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let nombre= "Juan"
+let edad= "40";
+let trabaja= True
+
+let info = {
+    marca: "Toyota",
+    modelo: "Yanis",
+    year: 2015
+
+let count = 3;
+while (count>= 0){
+    console.log(count)
+    count = count -1;
+}
+
+if (edad >=18){
+	console.log("mayor de edad");
+}else{
+	console.log("menor de edad")
+}
+
+let parrafo= document.createElement("p");
+parrafo.text= "Hola";
+parrafo.style.color= "red";
+document.body.appendChild(parrafo);
+
+
+}
